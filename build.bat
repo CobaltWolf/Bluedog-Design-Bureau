6b3769efe9c07a6fdd472b98a77097edbc2fe8c5
jsolson <olsonjs@gmail.com>
2016-06-14 22:10:57 -0400

    Build script

--- build.bat
+++ build.bat
@@ -3,4 +3,5 @@ Set BUILD=%date:~4,2%_%date:~7,2%_%date:~10,4%
 pause Press CRTL-C to quit now
 del BDB_*.zip
 7z a BDB_FULL_%BUILD%.zip @bdb_list_full.txt
+7z a BDB_SCI_PROBES_%BUILD%.zip @bdb_list_sci_probes.txt
 pause
