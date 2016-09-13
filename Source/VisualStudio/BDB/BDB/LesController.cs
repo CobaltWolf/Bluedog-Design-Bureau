a03f464f601bfa23d4d6edefcb24f2b4ac27a79e
jsolson <olsonjs@gmail.com>
2016-09-13 17:16:52 -0400

    Apollo LES - Add Canards Thanks Kurld!

--- Source/VisualStudio/BDB/BDB/LesController.cs
+++ Source/VisualStudio/BDB/BDB/LesController.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using UnityEngine;
-using KSP;
 
 // Portions copied with permission from PEBKAC Industries: Launch Escape System by Kurld
 // https://github.com/Jurld/PebkacLaunchEscape
@@ -14,6 +11,9 @@ namespace BDB
 {
     public class ModuleBdbLesController : PartModule
     {
+
+        private static string _myModTag = "[BDB Apollo]";
+
         [KSPField(isPersistant = false, guiActive = false)]
         public string escapeEngineID = "LES_Escape";
 
@@ -29,7 +29,7 @@ namespace BDB
         [KSPField(isPersistant = true, guiActiveEditor = true, guiName = "Pitch Motor Run Time")]
         [UI_FloatRange(minValue = 0, stepIncrement = 0.1f, maxValue = 10)]
         public float pitchEngineRunTime = 1.0f;
-
+        
         private double pitchEngineStartTime = 0;
 
         [KSPField(isPersistant = false, guiActive = false)]
@@ -53,18 +53,102 @@ namespace BDB
         private double _yaw;
         private double _pitch;
 
+        #region Fun With Canards!
+
+        private double _maxFuel;
+        private Vector3 _origComOffset;
+        private Vector3 _origCopOffset;
+        private Vector3 _origColOffset;
+
+        // GUI enabled for tuning purposes
+        [KSPField(isPersistant = true, guiActive = true, guiActiveEditor = true, guiName = "CoM multiplier")]
+        [UI_FloatRange(minValue = 1, stepIncrement = 1.0f, maxValue = 100)]
+        public float comMult = 90.0f;
+
+        // GUI enabled for tuning purposes
+        [KSPField(isPersistant = true, guiActive = true, guiActiveEditor = true, guiName = "CoP multiplier")]
+        [UI_FloatRange(minValue = 1, stepIncrement = 1.0f, maxValue = 100)]
+        public float copMult = 90.0f;
+
+        // time when the canards will deploy
+        private double _canardDeployTime;
+        private bool _deployed;
+
+        //the animation for the canards
+        private ModuleAnimateGeneric _deployAnimation;
+
+        // the lifting surface for the deployed canards
+        private ModuleLiftingSurface _liftingSurface;
+
+        private ModuleAnimateGeneric GetDeployAnimation()
+        {
+            Debug.Log(string.Format("{0} GetDeployAnimation", _myModTag));
+            ModuleAnimateGeneric myAnimation = null;
+
+            try
+            {
+                myAnimation = part.FindModulesImplementing<ModuleAnimateGeneric>().SingleOrDefault();
+            }
+            catch (System.Exception x)
+            {
+                Debug.LogError(string.Format("{0} ERROR: {1}", _myModTag, x.Message));
+            }
+
+            if (!myAnimation)
+            {
+                // this shouldn't happen under normal circumstances
+                Debug.LogError(string.Format("{0} ERROR: {1}", _myModTag, "Didn't find ModuleAnimateGeneric on LES!"));
+            }
+
+            return myAnimation;
+        }
+
+        private ModuleLiftingSurface GetLiftingSurface()
+        {
+            Debug.Log(string.Format("{0} ModulePebkacLesPitchControl.GetLiftingSurface", _myModTag));
+            ModuleLiftingSurface myLiftingSurface = null;
+
+            try
+            {
+                myLiftingSurface = part.FindModulesImplementing<ModuleLiftingSurface>().SingleOrDefault();
+            }
+            catch (System.Exception x)
+            {
+                Debug.Log(string.Format("{0} ERROR: {1}", _myModTag, x.Message));
+            }
+
+            if (!myLiftingSurface)
+            {
+                // this shouldn't happen under normal circumstances
+                Debug.LogError("ERROR: Didn't find ModuleLiftingSurface on LES nosecone!");
+            }
+
+            return myLiftingSurface;
+        }
+
+        #endregion
+
         public override void OnStart(StartState state)
         {
+
+            // hook up to the part attach callback
+            if (state == StartState.Editor)
+            {
+                part.OnEditorAttach += OnEditorAttach;
+            }
+
             List<ModuleEngines> engines = part.FindModulesImplementing<ModuleEngines>();
             foreach (ModuleEngines e in engines)
             {
                 if (e.engineID == escapeEngineID)
                 {
                     escapeEngine = e;
-                } else if (e.engineID == pitchEngineID)
+                }
+                else if (e.engineID == pitchEngineID)
                 {
                     pitchEngine = e;
-                } else if (e.engineID == jettisonEngineID)
+                }
+                else if (e.engineID == jettisonEngineID)
                 {
                     jettisonEngine = e;
                 }
@@ -86,66 +170,24 @@ namespace BDB
 
             // check for nulls in engines
 
-            part.Resources["SolidFuel"].isTweakable = false;
-            part.Resources["SolidFuel"].isVisible = false;
-        }
+            // set up the variables used to shift aerodynamics
+            _maxFuel = part.Resources["SolidFuel"].maxAmount;
+            _origComOffset = part.CoMOffset;
+            _origCopOffset = part.CoPOffset;
+            _origColOffset = part.CoLOffset;
 
-        [KSPAction("Abort!", actionGroup = KSPActionGroup.Abort)]
-        public void activateAbortAction(KSPActionParam param)
-        {
-            if (vessel.altitude < 3000)
-            {
-                activateAbortMode1aAction(param);
-            } else
-            {
-                activateAbortMode1bAction(param);
-            }
-        }
+            // set up the variables used by code for simming the canards
+            _deployAnimation = GetDeployAnimation();
+            _liftingSurface = GetLiftingSurface();
 
-        // https://en.wikipedia.org/wiki/Apollo_abort_modes
-        // https://www.hq.nasa.gov/alsj/CSM15_Launch_Escape_Subsystem_pp137-146.pdf
-
-        [KSPAction("Abort Mode 1A")]
-        public void activateAbortMode1aAction(KSPActionParam param)
-        {
-            Debug.Log("Abort Mode 1A Activated");
-            _aborted = true;
-            activateEscapeEngine();
-            activatePitchEngine();
-        }
-
-        [KSPAction("Abort Mode 1B")]
-        public void activateAbortMode1bAction(KSPActionParam param)
-        {
-            Debug.Log("Abort Mode 1B Activated");
-            _aborted = true;
-            activateEscapeEngine();
-        }
-
-        [KSPAction("Jettison Tower")]
-        public void activateJettisonAction(KSPActionParam param)
-        {
-            doJettison();
-        }
-
-        public override void OnActive()
-        {
-            doJettison();
-        }
-
-        [KSPEvent(guiName = "Jettison Tower", guiActive = true)]
-        public void doJettison()
-        {
-            Debug.Log("Jettison Tower Activated");
-            _hasJettisoned = true;
-            activatePitchEngine();
-            activateJettisonEngine();
-            if (part.Modules.Contains("ModuleDecouple"))
-            {
-                part.FindModulesImplementing<ModuleDecouple>().FirstOrDefault().Decouple();
-            }
         }
         
+        private void OnEditorAttach()
+        {
+            Debug.Log(string.Format("{0} LES OnEditorAttach", _myModTag));
+            part.transform.Rotate(0, -90, 0);
+        }
+
         public void FixedUpdate()
         {
             if (!HighLogic.LoadedSceneIsFlight || vessel.HoldPhysics)
@@ -155,11 +197,32 @@ namespace BDB
 
             if (escapeEngine != null && escapeEngine.EngineIgnited)
             {
-                if(Planetarium.GetUniversalTime() - escapeEngineStartTime >= escapeEngineRunTime)
+                if (Planetarium.GetUniversalTime() - escapeEngineStartTime >= escapeEngineRunTime)
                 {
                     escapeEngine.Shutdown();
                 }
+
+                // in real life, the LES had a huge chunk of depleted uranium ballast in its nosecone
+                // in the game, when we model the LES using a single part the center of mass shifts 
+                // too far aft as solid fuel is burned, compared to real life. This makes it 
+                // difficult to maintain stable flight
+
+                // while the escape engine is running, adjust the center of mass offset
+                // until 1.2 hits, we also have to adjust the center of pressure offset since CoP 
+                // is currently incorrectly tied to the final center of mass, rather than the part origin
+                var comFactor = (_maxFuel - part.Resources["SolidFuel"].amount) / comMult;
+                var copFactor = (_maxFuel - part.Resources["SolidFuel"].amount) / copMult;
+
+                var newComY = _origComOffset.y + (float)comFactor;
+                var newCopY = _origCopOffset.y + (float)copFactor;
+
+                part.CoMOffset.Set(_origComOffset.x, newComY, _origComOffset.z);
+                part.CoPOffset.Set(_origCopOffset.x, -newCopY, _origCopOffset.z);
+
+                //Debug.Log(string.Format("CoMOffset updated to {0}", newY.ToString()));
+
             }
+
             if (pitchEngine != null && pitchEngine.EngineIgnited)
             {
                 if (Planetarium.GetUniversalTime() - pitchEngineStartTime >= pitchEngineRunTime)
@@ -167,6 +230,7 @@ namespace BDB
                     pitchEngine.Shutdown();
                 }
             }
+
             if (jettisonEngine != null && jettisonEngine.EngineIgnited)
             {
                 if (Planetarium.GetUniversalTime() - jettisonEngineStartTime >= jettisonEngineRunTime)
@@ -174,13 +238,61 @@ namespace BDB
                     jettisonEngine.Shutdown();
                 }
             }
-            if (_aborted && checkCanAutoJettison())
+
+            if (_aborted && !_deployed && (Planetarium.GetUniversalTime() >= _canardDeployTime))
             {
-                doJettison();
+                // pop the canards
+                DeployCanards();
+            }
+
+            if (_aborted && CheckCanAutoJettison())
+            {
+                DoJettison();
             }
         }
         
-        private void activateEscapeEngine()
+        #region Abort!
+
+        [KSPAction("Abort!", actionGroup = KSPActionGroup.Abort)]
+        public void ActivateAbortAction(KSPActionParam param)
+        {
+            if (_aborted == false)
+            {
+                if (vessel.altitude < 3000)
+                {
+                    ActivateAbortMode1aAction(param);
+                }
+                else
+                {
+                    ActivateAbortMode1bAction(param);
+                }
+
+                // set the game time when the canards are to pop:
+                _canardDeployTime = escapeEngineStartTime + 11;
+            }
+        }
+
+        // https://en.wikipedia.org/wiki/Apollo_abort_modes
+        // https://www.hq.nasa.gov/alsj/CSM15_Launch_Escape_Subsystem_pp137-146.pdf
+
+        [KSPAction("Abort Mode 1A")]
+        public void ActivateAbortMode1aAction(KSPActionParam param)
+        {
+            Debug.Log("Abort Mode 1A Activated");
+            _aborted = true;
+            ActivateEscapeEngine();
+            ActivatePitchEngine();
+        }
+
+        [KSPAction("Abort Mode 1B")]
+        public void ActivateAbortMode1bAction(KSPActionParam param)
+        {
+            Debug.Log("Abort Mode 1B Activated");
+            _aborted = true;
+            ActivateEscapeEngine();
+        }
+
+        private void ActivateEscapeEngine()
         {
             if (escapeEngineStartTime <= 0)
             {
@@ -188,8 +300,8 @@ namespace BDB
                 escapeEngine.Activate();
             }
         }
-        
-        private void activatePitchEngine()
+
+        private void ActivatePitchEngine()
         {
             if (pitchEngineStartTime <= 0)
             {
@@ -197,19 +309,65 @@ namespace BDB
                 pitchEngine.Activate();
             }
         }
-        
-        private void activateJettisonEngine()
+
+        private void DeployCanards()
         {
-            if (jettisonEngineStartTime <= 0)
+            // start the animation and adjust aero for deployed canards
+            Debug.Log(string.Format("{0} Deploying canards", _myModTag));
+             if (_deployAnimation != null)
             {
-                jettisonEngineStartTime = Planetarium.GetUniversalTime();
-                jettisonEngine.Activate();
+                _deployAnimation.Toggle();
+            }
+           
+            Debug.Log(string.Format("{0} setting CoMOffset in Y axis from {1} to {2}", _myModTag, part.CoMOffset.y.ToString(), _origComOffset.y.ToString()));
+            Debug.Log(string.Format("{0} setting CoPOffset in Y axis from {1} to {2}", _myModTag, part.CoPOffset.y.ToString(), _origCopOffset.y.ToString()));
+
+            part.CoMOffset.Set(_origComOffset.x, _origComOffset.y, _origComOffset.z);
+            part.CoPOffset.Set(_origCopOffset.x, _origCopOffset.y, _origCopOffset.z);
+            part.CoLOffset.Set(_origColOffset.x, _origColOffset.y + 1.45f, _origColOffset.z);
+            
+            if (_liftingSurface != null && part.Modules.Contains<ModuleLiftingSurface>())
+            {
+                _liftingSurface.useInternalDragModel = true;
+                _liftingSurface.deflectionLiftCoeff = 0.35f;
+            }
+            
+            _deployed = true;
+            
+        }
+
+        #endregion
+
+        #region Jettison
+
+        // fires when AG is triggered
+        [KSPAction("Jettison Tower")]
+        public void ActivateJettisonAction(KSPActionParam param)
+        {
+            DoJettison();
+        }
+
+        // fires when part is staged
+        public override void OnActive()
+        {
+            DoJettison();
+        }
+
+        [KSPEvent(guiName = "Jettison Tower", guiActive = true)]
+        public void DoJettison()
+        {
+            Debug.Log("Jettison Tower Activated");
+            _hasJettisoned = true;
+            ActivateJettisonEngine();
+            if (part.Modules.Contains("ModuleDecouple"))
+            {
+                part.FindModulesImplementing<ModuleDecouple>().FirstOrDefault().Decouple();
             }
         }
 
-        private bool checkCanAutoJettison()
+        private bool CheckCanAutoJettison()
         {
-            if (!_aborted || _hasJettisoned || Planetarium.GetUniversalTime() - escapeEngineStartTime > 20)
+            if (!_aborted || _hasJettisoned) // || Planetarium.GetUniversalTime() - escapeEngineStartTime > 20)
             {
                 return false;
             }
@@ -225,5 +383,17 @@ namespace BDB
                 return _yaw > 175d && _pitch > 175d;
             }
         }
+
+        private void ActivateJettisonEngine()
+        {
+            if (jettisonEngineStartTime <= 0)
+            {
+                jettisonEngineStartTime = Planetarium.GetUniversalTime();
+                jettisonEngine.Activate();
+            }
+        }
+        
+        #endregion
+
     }
 }
