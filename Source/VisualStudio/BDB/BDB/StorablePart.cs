73b3f2eca58ac825554edb550486a73b0a74ba7e
Rodg88 <rodgerjss@gmail.com>
2022-08-19 10:54:04 +1000

    BDBStorablePart

---
+++ Source/VisualStudio/BDB/BDB/StorablePart.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using KSP.IO;
+using FinePrint;
+using Upgradeables;
+using KSP.UI.Screens;
+using KSP.Localization;
+using System.IO;
+
+// Taken from Angel-125's Sandcastle
+// https://github.com/Angel-125/Sandcastle
+
+namespace BDB
+{
+    public class ModuleBdbStorablePart : ModuleCargoPart
+    {
+        public override string GetInfo()
+        {
+            StringBuilder info = new StringBuilder();
+
+            info.AppendLine(Localizer.Format("#LOC_BDB_storablePartDescription"));
+            info.AppendLine(" ");
+            info.AppendLine(Localizer.Format("#LOC_BDB_storablePartDryMass", new string[] { string.Format("{0:n3}", part.mass) }));
+            info.AppendLine(Localizer.Format("#LOC_BDB_storablePartPackedVolume", new string[] { string.Format("{0:n1}", packedVolume) }));
+            if (stackableQuantity > 1)
+                info.AppendLine(Localizer.Format("#LOC_BDB_storablePartStackingCapacity", stackableQuantity.ToString()));
+
+            return info.ToString();
+        }
+    }
+}
